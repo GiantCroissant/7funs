b6b07daedf0df51ad068e96a3592cc90b8d964da
ayo1103 <ayo1103@gmail.com>
2015-06-10 11:32:11 +0800

    Add showinfo storyboard Implement multiple storyboards

--- iOS/iOS-7funs/iOS-7funs/MainViewController.swift
+++ iOS/iOS-7funs/iOS-7funs/MainViewController.swift
@@ -7,13 +7,120 @@
 //
 
 import UIKit
+import Alamofire
+import RealmSwift
+
+class Dog: Object {
+    dynamic var name = ""
+    dynamic var owner: Person?
+}
+
+class Person: Object {
+    dynamic var id = 0
+    dynamic var name = ""
+    dynamic var birthdate = NSDate(timeIntervalSince1970: 1)
+    let dogs = List<Dog>()
+
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
 
 class MainViewController: UIViewController {
+
+    @IBAction func showInfo(sender: UIButton) {
+        var storyboard = UIStoryboard(name: "ShowInfo", bundle: nil)
+        var controller = storyboard.instantiateInitialViewController() as! UIViewController
+//        self.presentViewController(controller , animated: true, completion: nil)
+        self.navigationController?.pushViewController(controller, animated: true)
+    }
+
+    @IBAction func alamoFireTest(sender: UIButton) {
+        usageTest()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let author = Person()
+        author.id = 11
+//        author.id = NSUUID().UUIDString
+//        author.name = JSON.("")
+
+        let realm = Realm()
+
+        realm.write {
+            realm.add(author, update: true)
+        }
+
+        realm.write {
+            author.name = "Thomas Pynchon"
+        }
+
+
+        let theAuthor = realm.objects(Person)
+        println(theAuthor)
+    }
+
+    func usageTest() {
+        makingARequest()
+        responseHandling()
+        responseStringHandler()
+        chainedResponseHandlers()
+    }
+
+    func makingARequest() {
+        Alamofire.request(.GET, "http://httpbin.org/get")
+    }
+
+    func responseHandling() {
+        Alamofire.request(.GET, "http://httpbin.org/get", parameters: ["foo": "bar"])
+            .response { (request, response, data, error) in
+
+                println("--------------------------------------------------")
+                println("                  responseHandling                ")
+                println("--------------------------------------------------")
+                println(request)
+                println(response)
+                println(error)
+        }
+    }
+
+    func responseStringHandler() {
+        Alamofire.request(.GET, "http://httpbin.org/get")
+            .responseString { (_, _, string, _) in
+
+                println("--------------------------------------------------")
+                println("                  responseStringHandler           ")
+                println("--------------------------------------------------")
+                println(string)
+        }
+    }
+
+    func responseJSONHandler() {
+        Alamofire.request(.GET, "http://httpbin.org/get")
+            .responseJSON { (_, _, JSON, _) in
+                println("--------------------------------------------------")
+                println("                  responseJSONHandler             ")
+                println("--------------------------------------------------")
+                println(JSON)
+        }
+    }
+
+    func chainedResponseHandlers() {
+        Alamofire.request(.GET, "http://httpbin.org/get")
+            .responseString { (_, _, string, _) in
+                println("--------------------------------------------------")
+                println("                  chainedResponseHandlers 1       ")
+                println("--------------------------------------------------")
+                println(string)
+            }
+            .responseJSON { (_, _, JSON, _) in
+                println("--------------------------------------------------")
+                println("                  chainedResponseHandlers 2       ")
+                println("--------------------------------------------------")
+                println(JSON)
+        }
     }
 
     override func didReceiveMemoryWarning() {
