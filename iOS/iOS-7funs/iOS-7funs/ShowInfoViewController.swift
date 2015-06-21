e59278f77a263662370fe94d7c9132ae34f89340
ayo1103 <ayo1103@gmail.com>
2015-06-21 23:00:03 +0800

    Add TabBarViewController to ShowInfo.storyboard

--- iOS/iOS-7funs/iOS-7funs/ShowInfoViewController.swift
+++ iOS/iOS-7funs/iOS-7funs/ShowInfoViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ShowInfoViewController: UIViewController {
+class ShowInfoViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
