64e80c63893c501d29552336b9d9b80ad2210192
Ray Wang <apprentice@giantcroissant.com>
2015-06-22 22:46:28 +0800

    Attempt to reuse Recipes data for Videos section
    Has some constraint error during run time. Will find out and remove the error. It does not affect running for now.

---
+++ iOS/iOS-7funs/iOS-7funs/VideosDishTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  VideosDishTableViewCell.swift
+//  iOS-7funs
+//
+//  Created by Apprentice on 6/22/15.
+//  Copyright (c) 2015 giantcroissant. All rights reserved.
+//
+
+import UIKit
+
+class VideosDishTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dishImage: UIImageView!
+    
+    @IBOutlet weak var dishName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ iOS/iOS-7funs/iOS-7funs/VideosTableViewController.swift
@@ -0,0 +1,122 @@
+//
+//  VideosTableViewController.swift
+//  iOS-7funs
+//
+//  Created by Apprentice on 6/22/15.
+//  Copyright (c) 2015 giantcroissant. All rights reserved.
+//
+
+import UIKit
+
+import RealmSwift
+
+class VideosTableViewController: UITableViewController {
+    
+    var dishes = Realm().objects(Dish)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return dishes.count
+        //return 0
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("VideoDishCell", forIndexPath: indexPath) as! VideosDishTableViewCell
+
+        // Configure the cell...
+        
+        let dish = dishes[indexPath.row]
+        cell.dishName.text = dish.name
+
+        let dirPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        
+        let docsDir = dirPaths[0] as! String
+        let destinationPath = docsDir.stringByAppendingPathComponent(dish.image)
+        
+        cell.dishImage.image = loadImageFromPath(destinationPath)
+        
+        return cell
+    }
+    
+    func loadImageFromPath(path: String) -> UIImage? {
+        
+        let image = UIImage(contentsOfFile: path)
+        
+        if image == nil {
+            
+            println("missing image at: \(path)")
+        }
+        println("\(path)") // this is just for you to see the path in case you want to go to the directory, using Finder.
+        return image
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
